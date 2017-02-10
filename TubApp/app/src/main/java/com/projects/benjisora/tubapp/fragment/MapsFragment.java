4164062d31227cfd345d6573de668b72b8982b47
PENVEN YANN p1406611 <yann.penven@etu.univ-lyon1.fr>
2017-02-10 17:34:57 +0100

    add kml draw

--- TubApp/app/src/main/java/com/projects/benjisora/tubapp/fragment/MapsFragment.java
+++ TubApp/app/src/main/java/com/projects/benjisora/tubapp/fragment/MapsFragment.java
@@ -2,13 +2,16 @@ package com.projects.benjisora.tubapp.fragment;
 
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,9 +27,13 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.kml.KmlLayer;
 import com.projects.benjisora.tubapp.R;
 import com.projects.benjisora.tubapp.ui.MainActivity;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -62,8 +69,8 @@ public class MapsFragment extends Fragment {
 
                 // For dropping a marker at a point on the Map
                 LatLng bourg = new LatLng(46.2053457, 5.2260777);
-                googleMap.addMarker(new MarkerOptions().position(bourg).title("Marker Title").snippet("Marker Description"));
-
+                //googleMap.addMarker(new MarkerOptions().position(bourg).title("Marker Title").snippet("Marker Description"));
+                drawKml("path1");
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(bourg).zoom(13).build();
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
@@ -71,6 +78,18 @@ public class MapsFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    public void drawKml(String kmlname){
+        Context context = getActivity().getApplicationContext();
+        Resources res = context.getResources();
+        int kmlId = res.getIdentifier(kmlname, "raw", context.getPackageName());
+        try{
+            KmlLayer layer = new KmlLayer(googleMap, kmlId, context);
+            layer.addLayerToMap();
+        }catch (IOException | XmlPullParserException e){
+            Log.d("MapsFragment", "drawKml: UNABLE TO LOAD KML");
+        }
     }
 
     @Override
